85356a6ed05a4112188fb516bb0ae71b2cc170b0
Ivan Dyedov <ivan@ivan-site.com>
2012-11-03 10:59:04 -0400

    add test for set & get, reference issue #1

---
+++ src/test/java/com/fiftyonred/mock_jedis/MockJedisTest.java
@@ -0,0 +1,29 @@
+package com.fiftyonred.mock_jedis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+
+public class MockJedisTest {
+	Jedis j;
+	
+	@Before
+	public void setUp() {
+		j = new MockJedis("test");
+	}
+	
+	@Test
+	public void testSet() {
+		assertEquals("OK", j.set("test", "123"));
+	}
+	
+	@Test
+	public void testGet() {
+		j.set("test", "123");
+		assertEquals("123", j.get("test"));
+		assertEquals(null, j.get("unknown"));
+	}
+}
